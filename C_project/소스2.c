629079b04d72b5adf910ce223646bbadb93115e1
juneshuai <67306008+juneshuai@users.noreply.github.com>
2020-11-26 00:39:32 +0900

    check

--- C_project/소스2.c
+++ C_project/소스2.c
@@ -2,18 +2,23 @@
 #include<stdlib.h>
 #include<string.h>
 typedef struct _studentList {
-	char name[10];
-	int id;
-	char major[20];
-	char grade[4];
-	int score;
+	char name[10]; //이름
+	int id; //학번
+	char major[20]; //학과
+	char grade[4]; // 학점
+	int score; //평점
+	struct _studentList *next;
 }studentList;
 
-
+void init();
 void readList();
 
 int main(void) {
-	int num;
+	
+	init();
+	/*int num;
+	
+	
 	while(1){
 		printf("================\n");
 		printf("1. 읽기\n");
@@ -30,34 +35,88 @@ int main(void) {
 		}
 	}
 	
-	return 0;
+	return 0;*/
 	
 }
 
+//0. 초기화
+void init() {
+	FILE *fp = NULL;
+	errno_t err;
+	err = fopen_s(&fp, "studentlist.txt", "r");
 
-//1. 읽기
+	if (err == 0) {
+		printf("파일 열기 성공\n");
+	}
+	else {
+		printf("파일 열기 실패\n");
+	}
+
+	studentList *head, *tail;
+	studentList *ptr;
+
+	head = tail = NULL;
+	studentList *dummy = (studentList *)malloc(sizeof(studentList));
+	head = dummy;
+	tail = dummy;
+	while (1) {
+		ptr = (studentList *)malloc(sizeof(studentList));
+		if (ptr == NULL)
+		{
+			printf("메모리 할당오류! \n");
+			exit(1);
+		}
+		fscanf_s("%s", ptr->name);
+		printf("%s", ptr->name);
+		break;
+	}
+	fclose(fp);
+
+}
+
+
+
+//1. 읽기(연결리스트 초기화)
 void readList() {
+	int cnt = 0; //라인 갯수세기
 	char str[100];
 	FILE *fp = NULL;
 	errno_t err;
 	err = fopen_s(&fp, "studentlist.txt", "r");
 
 	if (err == 0) {
-		printf("성공\n");
+		printf("파일 열기 성공\n");
 	}
 	else {
-		printf("실패\n");
+		printf("파일 열기 실패\n");
 	}
 	
 	while(!feof(fp)){
 		fgets(str, 100, fp);
 		printf("%s", str);
+		cnt++;
 	}
-
 	printf("\n");
+	printf("라인 갯수 : %d\n", cnt);
+	
+	
 	fclose(fp);
 	
 }
-//, , s1->grade, sizeof(s1->grade), s1->score, sizeof(s1->score)
+
+//2. 추가
+void addStudent() {
+	FILE *fp = NULL;
+	errno_t err;
+	err = fopen_s(&fp, "studentlist.txt", "a");
+	if (err == 0) {
+		printf("파일 열기 성공\n");
+	}
+	else {
+		printf("파일 열기 실패\n");
+	}
+
+	
+}
 
 
